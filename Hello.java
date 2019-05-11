57463181147196e2578a132a59f45d0daa8b22df
Pedro Frohmut <pedrofrohmut@gmail.com>
2019-05-11 10:49:29 -0300

    SpeedConverter

---
+++ Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+  public static void main(String[] args) {
+    System.out.println("Hello Pedro!");
+
+    int myFirstNumber = 5;
+    int mySecondNumber = 10;
+
+    System.out.println(myFirstNumber);
+
+    double sum = myFirstNumber + mySecondNumber;
+
+    System.out.println("Sum: " + sum);
+  }
+
+}
---
+++ MegaBytesConverter.java
@@ -0,0 +1,17 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(final int kilobytes) {
+        if (kilobytes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        final long mb = (long) Math.floor(kilobytes / 1024);
+        final long remainingKb = kilobytes - (mb * 1024);
+        System.out.println(kilobytes + " KB = " + mb + " MB and " + remainingKb + " KB");
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+}
